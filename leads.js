4ea53b6494d8dc758d360faaefcdb941e91d4c07
Quanda Grant <quandagrant@gmail.com>
2026-01-19 19:08:53 -0500

    update js and html

--- leads.js
+++ leads.js
@@ -1,9 +1,9 @@
-const url = "https://skogfghrpsiaibzbjgne.supabase.co/rest/v1/Leads";
+const url = "https://skogfghrpsiaibzbjgne.supabase.co/rest/v1/leads";
 const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrb2dmZ2hycHNpYWliemJqZ25lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3OTk1MDEsImV4cCI6MjA4MDM3NTUwMX0.Oli4Picy3nPZlxj0KUycXEyI2fu4AXiYJoKzg9TUh0Q";
 
 
 
-async function ceateleads() {
+async function createleads() {
             event.preventDefault();
 
             const id = document.getElementById('id').value;
@@ -12,27 +12,28 @@ async function ceateleads() {
 
             let leads = {
                  id,
-                email,
+                contact_email: email,
                 interest_messaging
             };
-            console.log(leads);
-
+          
+          
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'apikey': apikey,
                 },
-                body: JSON.stringify({ id, email, interest_messaging }),
+                body: JSON.stringify(leads),
             });
+            console.log(response);
             if(response.status !== 201){
                 document.getElementById('result').innerText = "Error creating lead: " + response.statusText;
                 return;
-            }else
+            }else{
 
             document.getElementById("result").innerText = "Lead created with ID: " + result.id;
         }
-
+    }
         async function getleads() {
             const response = await fetch(url, {
                 method: 'GET',
@@ -40,7 +41,7 @@ async function ceateleads() {
                     'Content-Type': 'application/json',
                     'apikey': apikey,
                 },
-                body: JSON.stringify({ id, email, interest_messaging }),
+                
             });
             const leads = await response.json();
             let leadsdata = document.getElementById('leadsdata');
@@ -48,35 +49,13 @@ async function ceateleads() {
             leads.forEach(lead => {
                 leadsdata.innerHTML += `<tr>
                 <td>${lead.id}</td>
-                <td>${lead.email}</td>
+                <td>${lead.contact_email}</td>
                 <td>${lead.interest_messaging}</td>
                 </tr>`;
             });
-            console.log(leads);
-        }
-        window.onload = function() {
-            getleads();
+          }
+   
 
-             async function getleads() {
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'apikey': apikey,
-                },
-                body: JSON.stringify({ id, email, interest_messaging }),
-        }
-          const leads = await response.json();
-            let leadsdata = document.getElementById('leadsdata');
-            leadsdata.innerHTML = '';
-            leads.forEach(lead => {
-                leadsdata.innerHTML += `<tr>
-                <td>${lead.id}</td>
-                <td>${lead.email}</td>
-                <td>${lead.interest_messaging}</td>
-                </tr>`;
-            });
-
-            
+       
         
-        
+               
